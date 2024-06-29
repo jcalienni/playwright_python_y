2de26f5ca24b394f948e29a17a25017bb3e7b732
jcalienni <juliancalienni@gmail.com>
2024-06-29 17:46:36 -0300

    video 3 locators assertions

--- test_herokuapp.py
+++ test_herokuapp.py
@@ -1,11 +1,12 @@
 import re
 from playwright.sync_api import Page, expect
 
+
 def test_verify_title(page: Page):
     page.goto("https://the-internet.herokuapp.com/")
 
     expect(page).to_have_title("The Internet")
-    # assert page.title() == "The Internet"
+
 
 def test_verify_checkboxes_url(page: Page):
     page.goto("https://the-internet.herokuapp.com/")
@@ -13,4 +14,37 @@ def test_verify_checkboxes_url(page: Page):
     page.get_by_role("link", name="Checkboxes").click()
 
     expect(page).to_have_url("https://the-internet.herokuapp.com/checkboxes")
-    # assert page.url == "https://the-internet.herokuapp.com/checkboxes"
+
+
+def test_verify_checkboxes_check(page: Page):
+    page.goto("https://the-internet.herokuapp.com/")
+
+    page.get_by_role("link", name="Checkboxes").click()
+    page.get_by_role("checkbox").first.check()
+    expect(page.get_by_role("checkbox").first).to_be_checked()
+
+
+def test_verify_checkboxes_uncheck(page: Page):
+    page.goto("https://the-internet.herokuapp.com/")
+
+    page.get_by_role("link", name="Checkboxes").click()
+    page.get_by_role("checkbox").nth(1).uncheck()
+    expect(page.get_by_role("checkbox").nth(1)).not_to_be_checked()
+
+
+def test_verify_input_text(page: Page):
+    page.goto("https://the-internet.herokuapp.com/")
+
+    page.get_by_role("link", name="Inputs").click()
+    page.locator("input").fill("123")
+    expect(page.locator("input")).to_have_value("123")
+
+
+def test_verify_text_in_page(page: Page):
+    page.goto("https://the-internet.herokuapp.com/")
+
+    page.get_by_role("heading", name="Available Examples")
+    expect(page.get_by_role("heading", name="Available Examples")
+           ).to_contain_text("able")
+    expect(page.get_by_role("heading", name="Available Examples")
+           ).to_have_text("Available Examples")
